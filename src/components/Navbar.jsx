e30c552af9e247d6788d6d424c4fc377c6e54aa9
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-01-30 21:44:14 +0530

    Added and Styled the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <div className={styles.callButton}>
+                    <Image src="/telephone.png" alt="telephone" width="32" height="32" />
+                </div>
+
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW!</div>
+                    <div className={styles.text}>012 345 678</div>
+                </div>
+            </div>
+
+            <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>Homepage</li>
+                    <li className={styles.listItem}>Products</li>
+                    <li className={styles.listItem}>Menu</li>
+                    <Image src="/logo3.png" alt="logo" width="160" height="96" />
+                    <li className={styles.listItem}>Events</li>
+                    <li className={styles.listItem}>Blog</li>
+                    <li className={styles.listItem}>Contact</li>
+                </ul>
+            </div>
+            
+            <div className={styles.item}>
+                <div className={styles.cart}>
+                    <Image src="/cart.png" alt="logo" width="30" height="30" />
+                    <div className={styles.counter}>2</div>
+                </div>
+            </div>
         </div>
     )
 }
