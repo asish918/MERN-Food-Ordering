35914350a4aa5c6dd032f677a9d7121e35d31bb9
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-02-01 23:33:50 +0530

    Made the Order Tracking Page Responsive

--- src/pages/orders/[id].jsx
+++ src/pages/orders/[id].jsx
@@ -17,7 +17,7 @@ const Order = () => {
                 <div className={styles.row}>
                     <table className={styles.table}>
                         <thead>
-                            <tr className={styles.tr}>
+                            <tr className={styles.trTitle}>
                                 <th>Order ID</th>
                                 <th>Customer</th>
                                 <th>Address</th>
@@ -25,7 +25,7 @@ const Order = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
+                            <tr className={styles.tr}>
                                 <td>
                                     <span className={styles.id}>1234512312</span>
                                 </td>
